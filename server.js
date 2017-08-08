a338ba927c369d7a684c92bda07a1cbf0f6479fc
nairananya <30752561+nairananya@users.noreply.github.com>
2017-08-08 19:56:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/Ananya Article',function(req,res){
+app.get('/Ananya',function(req,res){
     res.send('Ananya is busy');
 });
 app.get('/ui/style.css', function (req, res) {
